778e0d7c192f2591bd58db199f12513243c68ebb
Minard-NG <michael4sure20@gmail.com>
2021-12-10 07:02:43 +0100

    added product service

--- src/app/product-description/product-description.component.ts
+++ src/app/product-description/product-description.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { Album } from "../album";
+import { ProductService } from "../product.service";
+
 
 @Component({
   selector: 'app-product-description',
@@ -6,10 +9,13 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./product-description.component.css']
 })
 export class ProductDescriptionComponent implements OnInit {
-
-  constructor() { }
+  albumInfo: Album;
+  constructor(private _productService: ProductService) { }
 
   ngOnInit() {
+    this._productService
+      .getAlbum(1)
+      .subscribe((response) => (this.albumInfo = response));
   }
 
 }
